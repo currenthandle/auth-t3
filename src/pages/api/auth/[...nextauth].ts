46cda559a027b4c9db008c211fe23683f8c08788
Casey <leptone55@gmail.com>
2022-10-30 11:58:19 -0700

    auth set up

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,57 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+import CredentialsProvider from 'next-auth/providers/credentials'
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { env } from '../../../env/server.mjs'
+import { prisma } from '../../../server/db/client'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
+  // callbacks: {
+  //   session({ session, user }) {
+  //     if (session.user) {
+  //       session.user.id = user.id
+  //     }
+  //     return session
+  //   },
+  // },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
+
+  // pages: {
+  //   // signIn: '/auth/login',
+  //   signIn: '/login',
+  // },
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
+    CredentialsProvider({
+      name: 'Credentials',
+      // credentials: {
+      //   email: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+      //   password: { label: 'Password', type: 'password' },
+      // },
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials /*, req*/) {
+        if (credentials?.email === 't@t.com') {
+          const user = {
+            id: '3',
+            email: 't@t.com',
+          }
+          return user
+        }
+        return null
+      },
+    }),
   ],
-};
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
